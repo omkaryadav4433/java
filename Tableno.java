380881a3565f00a41f088080582cb12e0a374383
Omkar Yadav <130984348+omkaryadav4433@users.noreply.github.com>
2023-04-28 00:02:26 +0530

    Add files via upload

---
+++ Tableno.java
@@ -0,0 +1,21 @@
+package com;
+import java.util.Scanner;
+
+public class Tableno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        int i=1;
+        int n=sc.nextInt();
+        
+        for(int i1=1;i1<11;i1++) {
+        	System.out.println(i1*n);
+        }
+        
+        
+        
+        
+	}
+
+}
