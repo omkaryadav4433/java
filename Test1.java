758650f6c87fa4e1846c7a817575acc23bbd60dc
Omkar Yadav <130984348+omkaryadav4433@users.noreply.github.com>
2023-05-04 00:27:23 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,12 +1,20 @@
-package p2;
+package p1;
+
+import java.util.Scanner;
+//import ALL static members of the System class
+import static java.lang.System.*;
+import static java.lang.Math.sqrt;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		//direct ref.
-		ConsolePrinter printer=new ConsolePrinter();
-		printer.print("mesg1");
-
+		Scanner sc=new Scanner(in);
+		out.println("Enter complete name");
+		out.println("Hello "+sc.nextLine());
+		sc.close();
+		System.out.println(sqrt(25));
+		//how will you terminate JVM ?
+		exit(0);
 	}
 
 }
